dc11f4845c022a321b8d96ad55a5ec555c68e896
Thales Augusto Contratres <89500823+ThalesAuC@users.noreply.github.com>
2025-07-24 14:28:26 -0300

    att

--- main.py
+++ main.py
@@ -8,20 +8,27 @@ print(f)
 console = Console()
 console.print(Panel("1- Novo Cadastro"))
 console.print(Panel("2- Buscar Cadastro"))
-response = input(int());
+response = input(int())
 
 match response:
     case 1: #cadastrar novo usuário
     userid # ler a ultima id e aplicar a logica de +1 encima do ultimo cadastrado
-    nome = input("insira um nome para cadastro: ")
+    cpf = input("Insira o CPF: ")
+    nome = input("insira um nome: ")
     idade = int(input("insira idade: "))
     email = input("insira o e-mail: ")
 
 
     user = { #objeto usuário
         "id":userid,
+        "cpf":cpf,
         "nome":nome,
         "email":email,
     }
-
-print(json.dumps(user, indent=4, ensure_ascii=False))
+    print(json.dumps(user, indent=4, ensure_ascii=False))
+    
+    Case 2: #Buscar usuário já cadastrado para Deletar ou Atualizar
+    f = pyfiglet.figlet_format("Buscar Cadastro", font="slant")
+    print(f)
+    console = Console()
+    console.print(Panel("Insira uma informação para buscar o cadastro (ID,CPF ou Nome)"))
